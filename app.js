a704eb6112c2e428494d3a261a24cecbe7425908
Andrii Androsovych <kozakizdona@gmail.com>
2024-05-28 21:21:40 +0300

    jjgjgk

---
+++ app.js
@@ -0,0 +1,23 @@
+const https = require("https");
+const express = require("express");
+const fs = require("fs")
+const app = express();
+let options = {
+   key:  fs.readFileSync("andriiwebsites_click.key"),
+   cert: fs.readFileSync("andriiwebsites_click.crt")
+}
+
+app.use(express.static("public"));
+
+app.get("/help",(req,res)=>{
+    res.json({time:new Date().toLocaleTimeString()});
+})
+
+let server = https.createServer(options, app);
+
+// Handle server errors
+server.on('error', (err) => {
+    console.error('Server error:', err);
+});
+
+server.listen(443,()=>console.log("https Server listen on port 443..")); 
---
+++ public/scripts.js
@@ -0,0 +1,37 @@
+const videoEl = document.querySelector('#my-video');
+let stream = null;
+const constraints = {
+    audio: true,
+    video: true
+}
+
+const getMicAndCamera = async()=>{
+    try {
+         stream = await navigator.mediaDevices.getUserMedia(constraints);
+         alert("Stream created!")
+         console.log(stream);
+    } catch(e) {
+
+        alert("acces denied")
+       console.log(e)
+    }
+    
+}
+
+const showMyFeed = e=>{
+   alert("ShowMyFeed is working!");
+   videoEl.srcObject = stream;//this will set our MediaStream to our <Video/> tag 
+}
+
+const stopMyFeed = e=>{
+    alert("stop feed");
+    const tracks = stream.getTracks();
+    tracks.forEach(track=>{
+        track.stop(); //disassotiates a track with the source
+    })
+}
+//RogStrix G531GT-BQ132
+
+document.querySelector('#share').addEventListener('click',e=>getMicAndCamera(e));
+document.querySelector("#show-video").addEventListener('click',(e)=>showMyFeed(e))
+document.querySelector("#stop-video").addEventListener('click',(e)=>stopMyFeed(e))
